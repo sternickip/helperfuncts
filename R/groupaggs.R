586fff68ea4a8bec9e434630336ff66bcaadaced
unknown <sternicki.p@gmail.com>
2022-11-08 21:37:19 +0100

    adding documentation and exports

--- R/groupaggs.R
+++ R/groupaggs.R
@@ -1,3 +1,18 @@
+#' filter rows based on a column compared to a statistic within a group
+#'
+#' @param data dataset, must be a data.table
+#' @param group.cols columns to group by
+#' @param agg.funct function which value is calculated for the group
+#' @param agg.col numerical column which value is compared to the functions value
+#' @param symbol symbol of the comparison
+#'
+#' @return a filtered data.table object
+#' @export
+#'
+#' @examples
+#' #this returns rows of the iris set that have their Sepal.Length higher than the mean of their Species
+#' iris_dt <- as.data.table(iris)
+#' group.aggs(iris_dt,'Species',mean,'Sepal.Length','>')
 group.aggs <- function(data,group.cols,agg.funct,agg.col,symbol){
 
   stopifnot('data must be a data.table' = is.data.table(data))
@@ -6,16 +21,4 @@ group.aggs <- function(data,group.cols,agg.funct,agg.col,symbol){
   data[data[,.I[ get(symbol)(eval(str2lang(agg.col)), agg.funct(eval(str2lang(agg.col))))],by=group.cols]$V1]
 
 }
-#
-# library(data.table)
-# x <- as.data.table(iris)
-# group.aggs(x,'Species',mene,'Sepal.Length','>')
-#
-# ?`>`
-#
-#
-# stopifnot('data must be a data.table' = is.data.table(data))
-#
-# symbol <- c('==','>')
-#
-# stopifnot('symbol must be a relational operator' = symbol %in% getGroupMembers(Compare))
+
